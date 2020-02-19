47e0dae4e78869e42715bb8719f63bb096b4b411
Olex Lozitskiy <mr.olexander@gmail.com>
2020-02-19 00:40:12 -0600

    Added DXGI lib

--- LearningDX12/DX12App.cpp
+++ LearningDX12/DX12App.cpp
@@ -28,7 +28,7 @@ namespace Olex
         // so all possible errors generated while creating DX12 objects
         // are caught by the debug layer.
         Microsoft::WRL::ComPtr<ID3D12Debug> debugInterface;
-        ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+        ThrowIfFailed( D3D12GetDebugInterface( IID_PPV_ARGS( &debugInterface ) ) );
         debugInterface->EnableDebugLayer();
 #endif
     }
@@ -39,5 +39,50 @@ namespace Olex
         {
             throw std::exception();
         }
+    }
+
+    Microsoft::WRL::ComPtr<IDXGIAdapter4> DX12App::GetAdapter( bool useWarp )
+    {
+        using namespace Microsoft::WRL;
+
+        ComPtr<IDXGIFactory4> dxgiFactory;
+        UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+        createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+
+        ThrowIfFailed( CreateDXGIFactory2( createFactoryFlags, IID_PPV_ARGS( &dxgiFactory ) ) );
+
+        ComPtr<IDXGIAdapter1> dxgiAdapter1;
+        ComPtr<IDXGIAdapter4> dxgiAdapter4;
+
+        if ( useWarp )
+        {
+            ThrowIfFailed( dxgiFactory->EnumWarpAdapter( IID_PPV_ARGS( &dxgiAdapter1 ) ) );
+            ThrowIfFailed( dxgiAdapter1.As( &dxgiAdapter4 ) );
+        }
+        else
+        {
+            SIZE_T maxDedicatedVideoMemory = 0;
+            for ( UINT i = 0; dxgiFactory->EnumAdapters1( i, &dxgiAdapter1 ) != DXGI_ERROR_NOT_FOUND; ++i )
+            {
+                DXGI_ADAPTER_DESC1 dxgiAdapterDesc1;
+                dxgiAdapter1->GetDesc1( &dxgiAdapterDesc1 );
+
+                // Check to see if the adapter can create a D3D12 device without actually
+                // creating it. The adapter with the largest dedicated video memory
+                // is favored.
+                if ( ( dxgiAdapterDesc1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE ) == 0 &&
+                    SUCCEEDED( D3D12CreateDevice( dxgiAdapter1.Get(),
+                        D3D_FEATURE_LEVEL_11_0, __uuidof( ID3D12Device ), nullptr ) ) &&
+                    dxgiAdapterDesc1.DedicatedVideoMemory > maxDedicatedVideoMemory )
+                {
+                    maxDedicatedVideoMemory = dxgiAdapterDesc1.DedicatedVideoMemory;
+                    ThrowIfFailed( dxgiAdapter1.As( &dxgiAdapter4 ) );
+                }
+            }
+        }
+
+        return dxgiAdapter4;
     }
 }
--- LearningDX12/DX12App.h
+++ LearningDX12/DX12App.h
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <d3d12.h>
 #include <dxgi1_5.h>
+#include <dxgi1_6.h>
 #include <wrl/client.h>
 
 #include "framework.h"
@@ -16,51 +17,55 @@ namespace Olex
 
         void Init();
 
+    protected:
+
         void EnableDebugLayer();
 
         void ThrowIfFailed (HRESULT hr);
 
+        Microsoft::WRL::ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp);
+
     private:
 
         // The number of swap chain back buffers.
-        static constexpr uint8_t g_NumFrames = 3;
+        static constexpr uint8_t m_NumFrames = 3;
         // Use WARP adapter
-        bool g_UseWarp = false;
+        bool m_UseWarp = false;
 
-        uint32_t g_ClientWidth = 1280;
-        uint32_t g_ClientHeight = 720;
+        uint32_t m_ClientWidth = 1280;
+        uint32_t m_ClientHeight = 720;
 
         // Set to true once the DX12 objects have been initialized.
-        bool g_IsInitialized = false;
+        bool m_IsInitialized = false;
 
         // Window handle.
-        HWND g_hWnd;
+        HWND m_hWnd;
         // Window rectangle (used to toggle fullscreen state).
-        RECT g_WindowRect;
+        RECT m_WindowRect;
 
         // DirectX 12 Objects
-        Microsoft::WRL::ComPtr<ID3D12Device2> g_Device;
-        Microsoft::WRL::ComPtr<ID3D12CommandQueue> g_CommandQueue;
-        Microsoft::WRL::ComPtr<IDXGISwapChain4> g_SwapChain;
-        Microsoft::WRL::ComPtr<ID3D12Resource> g_BackBuffers[g_NumFrames];
-        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> g_CommandList;
-        Microsoft::WRL::ComPtr<ID3D12CommandAllocator> g_CommandAllocators[g_NumFrames];
-        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap;
-        UINT g_RTVDescriptorSize;
-        UINT g_CurrentBackBufferIndex;
+        Microsoft::WRL::ComPtr<ID3D12Device2> m_Device;
+        Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_CommandQueue;
+        Microsoft::WRL::ComPtr<IDXGISwapChain4> m_SwapChain;
+        Microsoft::WRL::ComPtr<ID3D12Resource> m_BackBuffers[m_NumFrames];
+        Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_CommandList;
+        Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_CommandAllocators[m_NumFrames];
+        Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> m_RTVDescriptorHeap;
+        UINT m_RTVDescriptorSize;
+        UINT m_CurrentBackBufferIndex;
 
         // Synchronization objects
-        Microsoft::WRL::ComPtr<ID3D12Fence> g_Fence;
-        uint64_t g_FenceValue = 0;
-        uint64_t g_FrameFenceValues[g_NumFrames] = {};
-        HANDLE g_FenceEvent;
+        Microsoft::WRL::ComPtr<ID3D12Fence> m_Fence;
+        uint64_t m_FenceValue = 0;
+        uint64_t m_FrameFenceValues[m_NumFrames] = {};
+        HANDLE m_FenceEvent;
 
         // By default, enable V-Sync.
         // Can be toggled with the V key.
-        bool g_VSync = true;
-        bool g_TearingSupported = false;
+        bool m_VSync = true;
+        bool m_TearingSupported = false;
         // By default, use windowed mode.
         // Can be toggled with the Alt+Enter or F11
-        bool g_Fullscreen = false;
+        bool m_Fullscreen = false;
     };
 }
