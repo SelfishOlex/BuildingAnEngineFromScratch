b9daa01552c6c79649cbfafe4da54d749f1c2e1f
AMZN-Olex <5432499+AMZN-Olex@users.noreply.github.com>
2023-03-26 14:39:56 -0500

    Bare bone vulkan main

--- LearningDX12/Main.cpp
+++ LearningDX12/Main.cpp
@@ -1,19 +1,9 @@
-//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
 
-#include "D3D12HelloTexture.h"
+#include <Renderer/Renderer.h>
 
-_Use_decl_annotations_
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
+int main()
 {
-    D3D12HelloTexture sample(1280, 720, L"D3D12 Hello Texture");
-    return Win32Application::Run(&sample, hInstance, nCmdShow);
+    Renderer renderer;
+    renderer.Start();
+    return 0;
 }
